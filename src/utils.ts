d673849c63638f5b8fa1477b3dcb2439eeff9ba8
Jerry Wester <jmanflanagan@gmail.com>
2021-04-04 04:03:37 -0600

    utils, findDLists

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+export function findDLists(file: Buffer): number[] {
+    const endDL: number[] = [];
+    for (let i = 0; i < file.length; i += 4) {
+        const possibleEnd = file.readUInt32BE(i);
+        if (possibleEnd === 0xDF000000 || possibleEnd === 0xDE010000) endDL.push(i);
+    }
+    const startDL: number[] = [];
+    for (let i = 0; i < endDL.length; i++) {
+        let offset = endDL[i];
+        while (file.readUInt8(offset - 8) <= 0x07 && file.readUInt8(offset - 8) >= 0xD3) {
+            offset -= 8;
+        }
+        startDL.push(offset);
+    }
+    return startDL;
+}
